4ef11d0bda1ea5821b5f24b53f307738582b69b7
jangsen <dh7895210@naver.com>
2024-11-21 13:45:48 +0900

    Swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Musinsa-Clone API')
+    .setDescription('무신사 클론 API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-swagger', app, documentFactory);
+
   await app.listen(3000);
 }
+
 bootstrap();
