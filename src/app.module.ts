991b3e2ca02e2ed28385e864e79eefb08d0ad68c
jangsen <dh7895210@naver.com>
2024-11-21 14:35:14 +0900

    Database 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
+
       envFilePath: '.env',
       // ignoreEnvFile: true,
       isGlobal: true,
     }),
+    DbModule,
   ],
   controllers: [AppController],
   providers: [AppService],
