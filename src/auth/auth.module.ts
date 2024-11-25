438ec40cba927cbf992625f577d0dcfaf9e1f514
jangsen <dh7895210@naver.com>
2024-11-25 15:20:58 +0900

    MailModule 임포트

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
 import { UserModule } from '@user/user.module';
+import { MailModule } from '@mail/mail.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MailModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
